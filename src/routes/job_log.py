9bc2fcd75f301f825c85da942078031b3ebdf873
Dragon-GCS <878173121@qq.com>
2024-07-01 20:53:44 +0800

    :zap: Pagination for large job logs

--- src/routes/job_log.py
+++ src/routes/job_log.py
@@ -1,10 +1,13 @@
-from typing import Literal
+from functools import lru_cache
+from typing import Annotated, Literal
 
 from fastapi import APIRouter
 from fastui import FastUI
 from fastui import components as c
 from fastui.components.forms import FormField, FormFieldSelect, FormFieldSelectSearch
 from fastui.events import GoToEvent
+from fastui.forms import SelectOption
+from pydantic import Field
 
 from ..config import LOG_PATH
 from ..log import PARSE_PATTERN, logger
@@ -12,6 +15,8 @@ from ..shared import frame_page
 from .api import get_available_job_logs
 
 router = APIRouter(prefix="/job/log", tags=["job_log"])
+
+PAGE_LINE = 1000
 
 
 def parse_log_message(text: str):
@@ -21,22 +26,23 @@ def parse_log_message(text: str):
     return messages[0]
 
 
-def log_content_to_markdown(log_name: str | None, level: str):
-    if not (log_name and (LOG_PATH / log_name).exists()):
-        return "Log not found"
-    contents = "\n\n".join(
-        [
-            f"**[{line['pid']}] {line['time']}** *{line['level']}* "
-            f"`{line['name']}:{line['line']}`: {parse_log_message(line['message'])}"
-            for line in logger.parse(LOG_PATH / log_name, pattern=PARSE_PATTERN)
-            if not level or level == line["level"]
-        ][::-1]
-    )
-    return contents
+@lru_cache(maxsize=100)
+def get_log_content(log_file: str, level: str):
+    return [
+        f"**[{line['pid']}] {line['time']}** *{line['level']}* "
+        f"`{line['name']}:{line['line']}`: {parse_log_message(line['message'])}"
+        for line in logger.parse(LOG_PATH / log_file, pattern=PARSE_PATTERN)
+        if (not level or level == line["level"])
+    ]
 
 
 @router.get("/{kind}", response_model=FastUI, response_model_exclude_none=True)
-def get_log(kind: Literal["jobs", "scheduler"], log_file: str | None = None, level: str = ""):
+async def get_log(
+    kind: Literal["jobs", "scheduler"],
+    log_file: str | None = None,
+    level: str = "",
+    page: Annotated[int, Field(ge=1)] = 1,
+):
     form_fields: list[FormField] = [
         FormFieldSelect(
             title="Level",
@@ -49,9 +55,14 @@ def get_log(kind: Literal["jobs", "scheduler"], log_file: str | None = None, lev
         )
     ]
     if kind == "jobs":
-        field_initial = get_available_job_logs().options[0]
-        if "options" in field_initial:
-            field_initial = field_initial["options"][0]
+        if not log_file:
+            field_initial = get_available_job_logs().options[0]
+            if "options" in field_initial:
+                field_initial = field_initial["options"][0]
+            log_file = field_initial["value"]
+        else:
+            field_initial = SelectOption(value=log_file, label=log_file)
+
         form_fields.append(
             FormFieldSelectSearch(
                 title="Log File",
@@ -60,10 +71,13 @@ def get_log(kind: Literal["jobs", "scheduler"], log_file: str | None = None, lev
                 initial=field_initial,
             ),
         )
-        log_file = field_initial["value"]
     else:
         log_file = "scheduler.log"
 
+    if not (log_file and (LOG_PATH / log_file).exists()):
+        return c.Error(title="File not found", description=f"Log file {log_file} not found.")
+
+    contents = get_log_content(log_file, level)
     return frame_page(
         c.Heading(text="Logs"),
         c.LinkList(
@@ -89,5 +103,10 @@ def get_log(kind: Literal["jobs", "scheduler"], log_file: str | None = None, lev
             submit_on_change=True,
             display_mode="inline",
         ),
-        c.Markdown(text=log_content_to_markdown(log_file, level), class_name="border rounded p-2"),
+        c.Pagination(page=page, page_size=PAGE_LINE, total=len(contents) or 1),
+        c.Markdown(
+            text="\n\n".join(contents[(page - 1) * PAGE_LINE : page * PAGE_LINE]),
+            class_name="border rounded p-2 mb-2",
+        ),
+        c.Pagination(page=page, page_size=PAGE_LINE, total=len(contents) or 1),
     )
